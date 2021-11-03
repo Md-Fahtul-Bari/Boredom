174a185bb246172783567d74933d970f3238debc
Md-Fahtul-Bari <fathul.bari.2004@gmail.com>
2021-11-03 10:55:19 +0600

    starting

---
+++ lib/mainScreens/randomstuffs.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class RandomStuffs extends StatelessWidget {
+  const RandomStuffs({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    const color = 0xFFEEF4F4;
+
+    return Scaffold(
+      backgroundColor: const Color(color),
+      body: Container(
+        child: Column(
+          children: const [
+            Text("asdasd"),
+          ],
+        ),
+      ),
+    );
+  }
+}
